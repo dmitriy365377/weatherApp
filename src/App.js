693ce8ba83878c1ca3fd2d3008d15f20e57c1c40
dmitriy365377 <tigr365377@gmail.com>
2020-05-01 17:39:03 +0300

    create addItem and deleteItem

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React,{useRef} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Form from './components/Form';
-import Weather from './components/Weather';
+import WeatherTest from './components/Weather';
 
 import GET_CURRENT_WEATHER from './queries/getCurrentWeather';
 import { useLazyQuery } from "@apollo/react-hooks";
@@ -9,13 +9,27 @@ import { useLazyQuery } from "@apollo/react-hooks";
 
 function App() {
   return (
-    <View />
+    <TestView />
+    // <View />
   );
 }
+ 
+function TestView() {
+  const [setCurrentWeather, { loading, called, data }] = useLazyQuery(GET_CURRENT_WEATHER, { fetchPolicy: 'network-only' });
+  const [items, setItems] = useState([]); 
+ 
 
-function View() {
-  const [setCurrentWeather, {loading, data}] = useLazyQuery(GET_CURRENT_WEATHER);
-  const childRef = useRef();
+  useEffect(() => {
+    if (data) {
+
+      const newItem = {
+        temp: data.getCurrentWeather.main.temp_c,
+        city: data.getWeather.city.name,
+        id: data.getWeather.city.id
+      }
+      setItems((prev) => [...prev, newItem]);
+    }
+  }, [data])
 
   const handelFormSubmmit = (e) => {
     e.preventDefault();
@@ -25,24 +39,67 @@ function View() {
       variables: {
         cityname: city
       }
-    });  
-    console.log(setCurrentWeather)
-    return childRef.current.addItem() 
-  }   
- 
+    });
+
+  }
+
+
+  const handleDeleteItem = (deleteId) => {
+    setItems((prev) => prev.filter(({ id }) => id !== deleteId));
+  }
+
   return (
     <div>
       <Form
         handelFormSubmmit={handelFormSubmmit}
       />
-      <Weather
-      ref={childRef} 
-      data={data} 
-      loading={loading} />
+      {
+        items.map(({ temp, city, id }) => (<>
+          <WeatherTest
+            id={id}
+            key={id}
+            temp={temp}
+            city={city}
+            onDelete={handleDeleteItem} />
+        </>))
+      }
     </div>
   )
 }
 
+
 export default App;
+
+
+// function View() {
+//   const [setCurrentWeather, {loading, data}] = useLazyQuery(GET_CURRENT_WEATHER);
+//   const childRef = useRef();
+
+//   const handelFormSubmmit = (e) => {
+//     e.preventDefault();
+//     const city = e.target.elements.city.value;
+
+//     setCurrentWeather({
+//       variables: {
+//         cityname: city
+//       }
+//     });  
+//     console.log(setCurrentWeather)
+//     return childRef.current.addItem() 
+//   }   
+ 
+//   return (
+//     <div>
+//       <Form
+//         handelFormSubmmit={handelFormSubmmit}
+//       />
+//       <Weather
+//       ref={childRef} 
+//       data={data} 
+//       loading={loading} />
+//     </div>
+//   )
+// }
+
 
 
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,49 +1,63 @@
-import React, { forwardRef, useImperativeHandle, useState,useEffect } from 'react';
+import React from 'react';
 
 
-const Weather = forwardRef((props, ref) => {
-    const [state, setState] = useState({ weatherData: [] });
-    if (props.loading) return <p>Loading ...</p>;
-
-    if (props.data && props.data.getCurrentWeather) {
-        console.log(props.data.getCurrentWeather);
-    } 
-
-    useImperativeHandle(ref, () => {
-        useEffect(() => {
-
-        })
-    });
-    // addItem() {
-    //     console.log('привет я оттуда');
-        
-    //     const newItem = {
-    //         temp: props.data.getCurrentWeather.main.temp_c,
-    //         city: props.data.getWeather.city.name,
-    //         id: props.data.getWeather.city.id
-    //     }
-
-    //     setState(({ weatherData }) => {
-    //         const newArray = [
-    //             ...weatherData,
-    //             newItem
-    //         ];
-
-    //         return {
-    //             weatherData: newArray
-    //         }
-    //     })
-    // } 
-    // useImperativeHandle(ref, () => ({
-    
-    // }));
-
+const WeatherTest = ({ temp, city, id, onDelete }) => {
     return (
         <div>
-            Weather Component
+            <button onClick={() => onDelete && onDelete(id)}>Удалить</button>
+            <div>{temp}</div>
+            <div>{city}</div>
+            <div>Id {id}</div>
         </div>
     )
-});
+}
 
 
-export default Weather;
+export default WeatherTest;
+
+
+ // import React, { forwardRef, useImperativeHandle, useState,useEffect } from 'react';
+// const Weather = forwardRef((props, ref) => {
+//     const [state, setState] = useState({ weatherData: [] });
+//     if (props.loading) return <p>Loading ...</p>;
+
+//     if (props.data && props.data.getCurrentWeather) {
+//         console.log(props.data.getCurrentWeather);
+//     } 
+
+//     // useImperativeHandle(ref, () => {
+//     //     useEffect(() => {
+
+//     //     })
+//     // });
+//     // addItem() {
+//     //     console.log('привет я оттуда');
+
+//     //     const newItem = {
+//     //         temp: props.data.getCurrentWeather.main.temp_c,
+//     //         city: props.data.getWeather.city.name,
+//     //         id: props.data.getWeather.city.id    
+//     //     }
+
+//     //     setState(({ weatherData }) => {
+//     //         const newArray = [
+//     //             ...weatherData,
+//     //             newItem
+//     //         ];
+
+//     //         return {
+//     //             weatherData: newArray
+//     //         }
+//     //     })
+//     // } 
+//     // useImperativeHandle(ref, () => ({
+
+//     // }));
+
+//     return (
+//         <div>
+//             Weather Component
+//         </div>
+//     )
+// });
+
