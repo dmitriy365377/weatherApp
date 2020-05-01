f892c2f314321bd3f797dc6d478e0b53ce64a10c
dmitriy365377 <tigr365377@gmail.com>
2020-05-01 08:14:44 +0300

    create css for serch input

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import './App.css';
- 
+import style from './App.module.css';
+
 import Form from './components/Form';
-import Weather from './components/Weather'; 
+import Weather from './components/Weather';
 
 import GET_CURRENT_WEATHER from './queries/getCurrentWeather';
-import { useLazyQuery  } from "@apollo/react-hooks";
+import { useLazyQuery } from "@apollo/react-hooks";
 
 
 function App() {
-  return ( 
-    <View /> 
+  return (
+    <View />
   );
-} 
+}
 
 function View() {
   const [setCurrentWeather, { data, error, loading }] = useLazyQuery(GET_CURRENT_WEATHER);
@@ -21,20 +21,25 @@ function View() {
     e.preventDefault();
     const city = e.target.elements.city.value;
     console.log(city)
-    setCurrentWeather({ variables: {
-      cityname:city
-     } }); 
-   
+    setCurrentWeather({
+      variables: {
+        cityname: city
+      }
+    });
+
   }
+
   console.log(data);
- 
+
 
   return (
     <div>
-      <Form handelFormSubmmit={handelFormSubmmit} />
+      <Form  
+      handelFormSubmmit={handelFormSubmmit} 
+      />
       <Weather />
     </div>
   )
-} 
+}
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,21 @@
 import React from 'react';
-
+import style from './Form.module.css'; 
 
 const Form = (props) => {
     return (
-        <form onSubmit={props.handelFormSubmmit}>
-            <input
-                type="text"
-                name="city"
-                placeholder="Enter city name..."
-            />
-            <button>Get Weather</button>
-        </form>
+        <div className={style.container}>
+            <form 
+            className={style.findlocation}
+            onSubmit={props.handelFormSubmmit}>
+                <input
+                    type="text"
+                    name="city"
+                    placeholder="Enter city name..."
+                />
+                {/* <button>Get Weather</button> */}
+                <input type="submit" value="Find"/>
+            </form>
+        </div>
     )
 }
 
